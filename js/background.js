bc7a7fc49fb37766c0ddfd90ba1e2e3ac1b2a885
Bryan Lai <bryanlais@gmail.com>
2018-12-30 14:56:59 +0800

    [Feature] Search in any language by language code

--- js/background.js
+++ js/background.js
@@ -1,17 +1,38 @@
 var options = {};
 var currentRequest = null;
+var currentLang = '';
 
-function updateOptions(default_lang) {
+var defaults = {
+    lang: 'en',
+    validLangs: [
+        "ab", "ace", "ady", "af", "ak", "als", "am", "an", "ang", "ar", "arc", "arz", "as", "ast", "atj", "av", "ay", "az", "azb", "ba", "bar", "bat-smg", "bcl", "be", "be-tarask", "bg", "bh", "bi", "bjn", "bm", "bn", "bo", "bpy", "br", "bs", "bug", "bxr", "ca", "cbk-zam", "cdo", "ce", "ceb", "ch", "chr", "chy", "ckb", "co", "cr", "crh", "cs", "csb", "cu", "cv", "cy", "da", "de", "din", "diq", "dsb", "dty", "dv", "dz", "ee", "el", "eml", "en", "eo", "es", "et", "eu", "ext", "fa", "ff", "fi", "fiu-vro", "fj", "fo", "fr", "frp", "frr", "fur", "fy", "ga", "gag", "gan", "gd", "gl", "glk", "gn", "gom", "gor", "got", "gu", "gv", "ha", "hak", "haw", "he", "hi", "hif", "hr", "hsb", "ht", "hu", "hy", "ia", "id", "ie", "ig", "ik", "ilo", "inh", "io", "is", "it", "iu", "ja", "jam", "jbo", "jv", "ka", "kaa", "kab", "kbd", "kbp", "kg", "ki", "kk", "kl", "km", "kn", "ko", "koi", "krc", "ks", "ksh", "ku", "kv", "kw", "ky", "la", "lad", "lb", "lbe", "lez", "lfn", "lg", "li", "lij", "lmo", "ln", "lo", "lrc", "lt", "ltg", "lv", "mai", "map-bms", "mdf", "mg", "mhr", "mi", "min", "mk", "ml", "mn", "mr", "mrj", "ms", "mt", "mwl", "my", "myv", "mzn", "na", "nah", "nap", "nds", "nds-nl", "ne", "new", "nl", "nn", "no", "nov", "nrm", "nso", "nv", "ny", "oc", "olo", "om", "or", "os", "pa", "pag", "pam", "pap", "pcd", "pdc", "pfl", "pi", "pih", "pl", "pms", "pnb", "pnt", "ps", "pt", "qu", "rm", "rmy", "rn", "ro", "roa-rup", "roa-tara", "ru", "rue", "rw", "sa", "sah", "sat", "sc", "scn", "sco", "sd", "se", "sg", "sh", "shn", "si", "simple", "sk", "sl", "sm", "sn", "so", "sq", "sr", "srn", "ss", "st", "stq", "su", "sv", "sw", "szl", "ta", "tcy", "te", "tet", "tg", "th", "ti", "tk", "tl", "tn", "to", "tpi", "tr", "ts", "tt", "tum", "tw", "ty", "tyv", "udm", "ug", "uk", "ur", "uz", "ve", "vec", "vep", "vi", "vls", "vo", "wa", "war", "wo", "wuu", "xal", "xh", "xmf", "yi", "yo", "za", "zea", "zh", "zh-classical", "zh-min-nan", "zh-yue", "zu"
+    ]
+};
+
+function updateOptions(updated_lang) {
     try {
         options = JSON.parse(localStorage.options);
     } catch (e) {}
-    var defaults = {
-        lang: default_lang
-    };
-    for (var key in defaults) {
-        if (options[key] == undefined && defaults[key]) {
-            options[key] = defaults[key];
+
+    var updated = Object.assign({
+        lang: updated_lang
+    }, defaults); // Clone of defaults
+
+    for (var key in updated) {
+        if (options[key] == undefined && updated[key]) {
+            options[key] = updated[key];
         }
+    }
+}
+
+function processInput(text) {
+    words = text.split(/[ \t]+/).filter(entry => entry != '');
+    if (options.validLangs.includes(words[0])) {
+        currentLang = words[0];
+        return words.slice(1).join(' ');
+    } else {
+        currentLang = options.lang;
+        return text;
     }
 }
 
@@ -25,6 +46,8 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
         currentRequest.abort();
         currentRequest = null;
     }
+
+    text = processInput(text);
 
     updateDefaultSuggestion(text);
 
@@ -74,7 +97,7 @@ chrome.omnibox.onInputCancelled.addListener(function() {
 function suggests(query, callback) {
     var req = new XMLHttpRequest();
 
-    req.open("GET", "https://" + options.lang + ".wikipedia.org/w/api.php?action=opensearch&namespace=0&suggest=&search=" + query, true);
+    req.open("GET", "https://" + currentLang + ".wikipedia.org/w/api.php?action=opensearch&namespace=0&suggest=&search=" + query, true);
     req.onload = function() {
         if (this.status == 200) {
             try {
@@ -94,6 +117,6 @@ function suggests(query, callback) {
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
     chrome.tabs.update(null, {
-        url: "https://" + options.lang + ".wikipedia.org/w/index.php?search=" + text
+        url: "https://" + currentLang + ".wikipedia.org/w/index.php?search=" + text
     });
 });
--- js/options.js
+++ js/options.js
@@ -3,12 +3,7 @@ try {
     data = JSON.parse(localStorage.options);
 } catch (e) {}
 
-var defaults = {
-    lang: '',
-    validLangs: [
-        "ab", "ace", "ady", "af", "ak", "als", "am", "an", "ang", "ar", "arc", "arz", "as", "ast", "atj", "av", "ay", "az", "azb", "ba", "bar", "bat-smg", "bcl", "be", "be-tarask", "bg", "bh", "bi", "bjn", "bm", "bn", "bo", "bpy", "br", "bs", "bug", "bxr", "ca", "cbk-zam", "cdo", "ce", "ceb", "ch", "chr", "chy", "ckb", "co", "cr", "crh", "cs", "csb", "cu", "cv", "cy", "da", "de", "din", "diq", "dsb", "dty", "dv", "dz", "ee", "el", "eml", "en", "eo", "es", "et", "eu", "ext", "fa", "ff", "fi", "fiu-vro", "fj", "fo", "fr", "frp", "frr", "fur", "fy", "ga", "gag", "gan", "gd", "gl", "glk", "gn", "gom", "gor", "got", "gu", "gv", "ha", "hak", "haw", "he", "hi", "hif", "hr", "hsb", "ht", "hu", "hy", "ia", "id", "ie", "ig", "ik", "ilo", "inh", "io", "is", "it", "iu", "ja", "jam", "jbo", "jv", "ka", "kaa", "kab", "kbd", "kbp", "kg", "ki", "kk", "kl", "km", "kn", "ko", "koi", "krc", "ks", "ksh", "ku", "kv", "kw", "ky", "la", "lad", "lb", "lbe", "lez", "lfn", "lg", "li", "lij", "lmo", "ln", "lo", "lrc", "lt", "ltg", "lv", "mai", "map-bms", "mdf", "mg", "mhr", "mi", "min", "mk", "ml", "mn", "mr", "mrj", "ms", "mt", "mwl", "my", "myv", "mzn", "na", "nah", "nap", "nds", "nds-nl", "ne", "new", "nl", "nn", "no", "nov", "nrm", "nso", "nv", "ny", "oc", "olo", "om", "or", "os", "pa", "pag", "pam", "pap", "pcd", "pdc", "pfl", "pi", "pih", "pl", "pms", "pnb", "pnt", "ps", "pt", "qu", "rm", "rmy", "rn", "ro", "roa-rup", "roa-tara", "ru", "rue", "rw", "sa", "sah", "sat", "sc", "scn", "sco", "sd", "se", "sg", "sh", "shn", "si", "simple", "sk", "sl", "sm", "sn", "so", "sq", "sr", "srn", "ss", "st", "stq", "su", "sv", "sw", "szl", "ta", "tcy", "te", "tet", "tg", "th", "ti", "tk", "tl", "tn", "to", "tpi", "tr", "ts", "tt", "tum", "tw", "ty", "tyv", "udm", "ug", "uk", "ur", "uz", "ve", "vec", "vep", "vi", "vls", "vo", "wa", "war", "wo", "wuu", "xal", "xh", "xmf", "yi", "yo", "za", "zea", "zh", "zh-classical", "zh-min-nan", "zh-yue", "zu"
-    ]
-};  // updated: 2018-12-30
+var defaults = chrome.extension.getBackgroundPage().defaults;
 
 var options = {};
 for (var id in defaults) {
